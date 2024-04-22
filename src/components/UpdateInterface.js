880f22aa7271b93303b50fbfa30bd66bbcd50d8f
小新 <782446723@qq.com>
2024-04-22 17:35:11 +0800

    feat: 响应式对象

--- src/components/UpdateInterface.js
+++ src/components/UpdateInterface.js
@@ -5,11 +5,18 @@ function UpdateInterface() {
     const handleClick = () => {
         setCount(count + 1);
     };
+    //如果是对象
+    const [form, setForm] = useState({ name: 'init' });
+    const changeForm = () => {
+        setForm({ ...form, name: 'change' });
+    };
     return (
         <div>
             <h1>更新界面</h1>
             <button onClick={handleClick}>点击按钮增加1</button>
+            <button onClick={changeForm}>点击修改对象</button>
             <h2>变量：{count}</h2>
+            <h2>对象: {form.name}</h2>
         </div>
     );
 }
