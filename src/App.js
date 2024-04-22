f421c2a4031f49f909a95c6516cc7c412c932c4b
小新 <782446723@qq.com>
2024-04-22 17:38:56 +0800

    feat: 分隔符组件

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import ListRenderingStudy from './components/ListRenderingStudy';
 import ConditionRenderingStudy from './components/ConditionalRenderingStudy';
 import EventBinding from './components/EventBinding';
 import UpdateInterface from './components/UpdateInterface';
+import Divider from './components/Divider';
 
 function App() {
     return (
         <div className='App'>
             <ListRenderingStudy />
+            <Divider />
             <ConditionRenderingStudy />
+            <Divider />
             <EventBinding />
+            <Divider />
             <UpdateInterface />
+            <Divider />
         </div>
     );
 }
---
+++ src/components/Divider.js
@@ -0,0 +1,5 @@
+function Divider() {
+    return <div style={{ borderBottom: '1px dashed #ccc', width: '100%', height: '0', margin: '16px 0' }} />;
+}
+
+export default Divider;
